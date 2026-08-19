dfa5ba92c4424d23ba93c9574e4d6609aa1ebee8
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2026-08-19 12:00:10 +0200

    Fix linting

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
-const { data: blog_posts } = await useFetch("https://jsonplaceholder.typicode.com/posts")
+const { data: blog_posts } = await useFetch('https://jsonplaceholder.typicode.com/posts')
 
 const page = ref(1)
 </script>
 
 <template>
   <div>
-    <h2 class="text-2xl font-semibold tracking-tight"></h2>
     <p>These are all the blog posts</p>
     <br>
     <UPageGrid>
-      <UPageCard variant="subtle" class="w-full" 
-        v-for="post in blog_posts" 
-        :key="post.id" 
+      <UPageCard
+        v-for="post in blog_posts"
+        variant="subtle"
+        :key="post.id"
+        class="w-full"
         :title="post.title"
         :description="post.body"
         :to="`/blog/post_${post.id}`"
       />
     </UPageGrid>
-    <UPagination v-model:page="page" :items-per-page="20" :total="100" />
+    <UPagination
+      v-model:page="page"
+      :items-per-page="20"
+      :total="100"
+    />
   </div>
 </template>
 
--- app/pages/blog/post_[id].vue
+++ app/pages/blog/post_[id].vue
@@ -1,37 +1,61 @@
 <script setup lang="ts">
-
 const route = useRoute()
 
-const { data: blog_post, error } = await useFetch(() => `https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+const { data: blog_post } = await useFetch(() => `https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
 
-const { data: comments } = await useFetch("https://jsonplaceholder.typicode.com/comments", {
-  query: { 
+const { data: comments } = await useFetch('https://jsonplaceholder.typicode.com/comments', {
+  query: {
     postId: route.params.id }
 })
 </script>
 
 <template>
-  <h2 class="text-2xl font-semibold tracking-tight">Blog post</h2>
-  <UCard variant="subtle" class="w-full" v-if="blog_post">
-    <template #header>
-      <h3 class="font-semibold">{{ blog_post.title }}</h3>
-    </template>
-    <p>{{ blog_post.body }}</p>
-  </UCard>
-  <p v-else="blog_post">Blog post with ID {{ route.params.id }} not found.</p>
-  <br>
-  <h2 class="text-2xl font-semibold tracking-tight">Comments</h2>
-  <UPageList v-if="comments">
-    <UPageCard v-for="comment in comments" :key="comment.id"
-      :title="comment.name"
-      :description="comment.body">
-      <template #footer>
-        <UUser :name="comment.email" avatar="{ alt: comment.email }"/>
+  <div>
+    <h2 class="text-2xl font-semibold tracking-tight">
+      Blog post
+    </h2>
+    <UCard
+      v-if="blog_post"
+      variant="subtle"
+      class="w-full"
+    >
+      <template #header>
+        <h3 class="font-semibold">
+          {{ blog_post.title }}
+        </h3>
       </template>
-    </UPageCard>
-  </UPageList>
-  <p v-else="comment">No comments.</p>
-
+      <p>
+        {{ blog_post.body }}
+      </p>
+    </UCard>
+    <p v-else>
+      Blog post with ID {{ route.params.id }} not found.
+    </p>
+    <br>
+    <h2 class="text-2xl font-semibold tracking-tight">
+      Comments
+    </h2>
+    <UPageList
+      v-if="comments"
+    >
+      <UPageCard
+        v-for="comment in comments"
+        :key="comment.id"
+        :title="comment.name"
+        :description="comment.body"
+      >
+        <template #footer>
+          <UUser
+            :name="comment.email"
+            avatar="{ alt: comment.email }"
+          />
+        </template>
+      </UPageCard>
+    </UPageList>
+    <p v-else>
+      No comments.
+    </p>
+  </div>
 </template>
 
 <style>
